719720365fb0b05fd824a35c22cf6e03b9ff1499
MuhamedTharik <126137677+MuhamedTharik@users.noreply.github.com>
2023-11-10 19:33:18 +0530

    Add files via upload

---
+++ prob12.js
@@ -0,0 +1,9 @@
+var addTwoPromises = async function(promise1, promise2) {
+    return new Promise((res,rej)=>{
+        promise1.then((val1)=>{
+            promise2.then((val2)=>{
+                return res(val1 + val2)
+            })
+        })
+    })
+};
