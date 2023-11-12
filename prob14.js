8845cf309d9f56f39fa8fdf9bf3f9bf9a8b5636e
MuhamedTharik <126137677+MuhamedTharik@users.noreply.github.com>
2023-11-12 11:46:51 +0530

    Add files via upload

---
+++ prob14.js
@@ -0,0 +1,8 @@
+var cancellable = function(fn, args, t) {
+    var timeout = setTimeout(() =>
+        fn(...args)
+    , t)
+    var cancelFn = () => clearTimeout(timeout);
+
+    return cancelFn;
+};
