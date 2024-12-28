cdd21bce688c7f797d3f09586cb9a56f1193ed67
MuhamedTharik <126137677+MuhamedTharik@users.noreply.github.com>
2024-12-28 09:20:23 +0530

    Delete prob12.js

--- prob12.js
+++
@@ -1,9 +0,0 @@
-var addTwoPromises = async function(promise1, promise2) {
-    return new Promise((res,rej)=>{
-        promise1.then((val1)=>{
-            promise2.then((val2)=>{
-                return res(val1 + val2)
-            })
-        })
-    })
-};
