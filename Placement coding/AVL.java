9f703d3bd8cf967705a3acd672f6d35d88d90104
MuhamedTharik <126137677+MuhamedTharik@users.noreply.github.com>
2025-03-24 09:55:34 +0530

    Update AVL.java

--- Placement coding/AVL.java
+++ Placement coding/AVL.java
@@ -11,21 +11,70 @@ class Node{
 }
 
 class AVL{
-    Node insert(Node root,int val){
-        if(root == null){
+    
+    int getheight(Node node){
+        return node==null? 0:node.ht;
+    }
+    
+    int getbalance(Node node){
+        return node==null? 0:(getheight(node.left) - getheight(node.right));
+    }
+    
+    Node left(Node y){
+        Node x = y.left;
+        x.right = y;
+        return x;
+    }
+        
+    Node right(Node y){
+        Node x = y.right;
+        x.left = y;
+        return x;
+    }
+    
+    Node leftright(Node y){
+        y.left = left(y.left);
+        return right(y);
+    }
+    Node rightleft(Node y){
+        y.right = right(y.right);
+        return left(y);
+    }
+    void inorder(Node node){
+        if(node != null){
+        inorder(node.left);
+        System.out.print(node.val+" ");
+        inorder(node.right);
+    }
+
+    }
+    
+    
+    Node insert(Node node,int val){
+        if(node == null){
             return new Node(val);
         }
-        else if(root.val>val){
-            root.left = insert(node.left,val);
+        else if(node.val>val){
+            node.left = insert(node.left,val);
         }
-        else if(root.val<val){
-            root.right = insert(node.right,val);
+        else if(node.val<val){
+            node.right = insert(node.right,val);
         }
         else
-        return root;
+        return node;
         
-        int ht = getheight(root);
-        int bal = getheight(root.left-root.right);
+        node.ht = Math.max(getheight(node.left),getheight(node.right))+1;
+        int bal = getbalance(node);
+        
+        if(bal > 1 && val<node.left.val)
+        Node x = right(Node y);
+        else if( bal <-1 && val>node.right.val)
+        Node x = left(Node y);
+        else if(bal<-1 && val<node.right.val)
+        Node x = leftright(Node y);
+        else if(bal >1 && val>node.left.val)
+        Node x = rightleft(Node y);
+        return node;
     }
 }
 
@@ -35,11 +84,11 @@ public class Main{
         Node root = null;
         int[] values ={20,30,25,47,67,90,33,89};
         for(int i:values){
-            root = insert(root,i);
+            root = t.insert(root,i);
         }
         t.inorder(root);
-        t.preorder(root);
-        t.postorder(root);
+        // t.preorder(root);
+        // t.postorder(root);
         
     }
 }
