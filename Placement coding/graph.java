5d2b5365694544b55fb6cab52922b968c2f9d034
MuhamedTharik <126137677+MuhamedTharik@users.noreply.github.com>
2025-04-08 14:41:26 +0530

    graph.java

---
+++ Placement coding/graph.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+class Graph{
+    int vertices;
+    ArrayList<ArrayList<Integer>> adj;
+    
+    Graph(int a){
+        adj = new ArrayList<>();
+        vertices = a;
+        for(int i=0;i<a;i++){
+            adj.add(new ArrayList<>());
+        }
+    }
+    
+    public void edge(int a,int b){
+        adj.get(a).add(b);
+        adj.get(b).add(a);
+    }
+    
+    public void display(){
+        for(int i=0;i<5;i++){
+            System.out.print("Adjacency list of"+i+"Index :");
+            for(int j=0;j<adj.get(i).size();j++){
+                System.out.print(adj.get(i).get(j)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class Main{
+    public static void main(String args[]){
+        Graph g = new Graph(5);
+        g.edge(0, 1);
+        g.edge(1, 4);
+        g.edge(1, 3);
+        g.edge(2, 4);
+        g.display();
+    }
+}
